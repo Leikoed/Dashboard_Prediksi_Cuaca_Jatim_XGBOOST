f62b0599f5077d228939f6253babe1db35daab09
Leikoed <168328345+Leikoed@users.noreply.github.com>
2026-06-26 00:36:41 +0700

    Delete 1_📊_EDA.py

--- 1_📊_EDA.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("📊 Exploratory Data Analysis")
-
-st.subheader("Distribusi Target & Pola Musiman")
-st.image("assets/eda_cuaca_jatim.png")
-
-st.subheader("Korelasi Fitur terhadap Target")
-st.image("assets/correlation_features.png")
